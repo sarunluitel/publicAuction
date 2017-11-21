79b2f262db0c66d16fed34573b75a045cebc20ee
Jacob Hurst <jhurst@unm.edu>
2017-11-20 21:55:33 -0700

    First connection made

--- src/AuctionCentral/AuctionCentralProtocol.java
+++ src/AuctionCentral/AuctionCentralProtocol.java
@@ -9,18 +9,25 @@
 package AuctionCentral;
 
 import AuctionHouse.AuctionHouse;
+
+import java.net.Socket;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class AuctionCentralProtocol {
+  private Socket socket = null;
+  private String name = null;
+  
   private static Map<String, AuctionHouse> auctionHouses = Collections.synchronizedMap(new HashMap<String, AuctionHouse>());
   
   private static final int WAITING = 0;
   private int state = WAITING;
   
-  public AuctionCentralProtocol()
+  public AuctionCentralProtocol(Socket socket, String name)
   {
+    this.socket = socket;
+    this.name = name;
     System.out.println("AuctionCentralProtocol-Constructor");
   }
   
@@ -34,6 +41,7 @@ public class AuctionCentralProtocol {
   
   public void registerAuctionHouse(AuctionHouse auctionHouse)
   {
+  
   }
   
   public void deregisterAuctionHouse(AuctionHouse auctionHouse)
