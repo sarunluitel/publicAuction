25b9024b38d032f72628abe6c0ff3866926d7fd4
Jacob Hurst <jhurst@unm.edu>
2017-11-20 20:28:18 -0700

    Setup

--- src/AuctionCentral.java
+++ src/AuctionCentral.java
@@ -8,6 +8,9 @@
  * transactions between bank and auction house.
  */
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,4 +19,22 @@ public class AuctionCentral
 {
   private static Map<String, AuctionHouse> auctionHouses = Collections.synchronizedMap(new HashMap<String, AuctionHouse>());
   private static String[] houseNames;
+  
+  public static void main(String args[]) throws IOException
+  {
+    ServerSocket serverSocket = new ServerSocket(1234);
+    Socket clientSocket = serverSocket.accept();
+  }
+  
+  /*
+  houseProtocol
+  agentProtocol
+  bankProtocol
+  
+  while(true)
+  {
+    accept a connection;
+    create a thread to deal with the client;
+  }
+   */
 }
