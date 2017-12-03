67d5cf5a1a0e8f5ca8355a5f4055d5af9fc36536
vhuber <vhuber@unm.edu>
2017-12-03 15:01:00 -0700

    test commit auctionhouse

--- src/AuctionHouse/AuctionHouse.java
+++ src/AuctionHouse/AuctionHouse.java
@@ -21,11 +21,53 @@ import java.util.Scanner;
 
 public class AuctionHouse
 {
+  public class Item
+  {
+    private String itemName;
+    private int agentKey;
+    private int bidAmount;
+
+
+    public Item(int num)
+    {
+      itemName = "Item" + num;
+      bidAmount = 100;
+    }
+
+    public String getItemName()
+    {
+      return itemName;
+    }
+
+    public int getBidAmount()
+    {
+      return bidAmount;
+    }
+
+    public int getAgent()
+    {
+      return agentKey;
+    }
+
+    private void setCurrentBid(int bid)
+    {
+      bidAmount = bid;
+    }
+
+    private void setAgentKey(int agent)
+    {
+      agentKey = agent;
+    }
+  }
+
   private final String name;
   private final int publicID;
   private static int totalHouses = 0;
   private LinkedList<String> itemList = new LinkedList<>();
   private LinkedList<Item> itemsForSale = new LinkedList<>();
+
+
+
 
   /**
    * Default constructor.
@@ -58,7 +100,9 @@ public class AuctionHouse
     for (int i = 1; i < 4; i++)
     {
       itemsForSale.add(new Item(i));
+      itemsForSale.get(i).setCurrentBid(100);
     }
+
   }
 
   /**
