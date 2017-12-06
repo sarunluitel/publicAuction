1e49cfba66becbe26f51f800402e84deeaa8b7d2
Jaehee Shin <jennyshin8@unm.edu>
2017-12-05 19:50:24 -0700

    Agent sends msgs to AuctionCentral

--- src/Agent/Agent.java
+++ src/Agent/Agent.java
@@ -144,6 +144,7 @@ public class Agent extends Thread implements Serializable
             if(bankIn.available() != 0) bankInput = ((Message) bankIn.readObject());
 
 //            bankInput = auctionInput = null;
+            
             if (!messageText.equals(""))
             {
               System.out.println(this.getAgentName() + "Submitting message = " + messageText + " to auction & bank.");
