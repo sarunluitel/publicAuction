ea3e28b9eaf2adafdd2ed0792228e0de6c32b438
vhuber <vhuber@unm.edu>
2017-12-07 20:03:26 -0700

    Bid acceptance

--- src/AuctionHouse/AuctionHouseProtocol.java
+++ src/AuctionHouse/AuctionHouseProtocol.java
@@ -37,7 +37,7 @@ public class AuctionHouseProtocol
         break;
       case "de-registered":
         message = "ignore";
-        response = new Message(this, house.getName() + ": ", message, "", -1, -1);
+        response = new Message(house, house.getName() + ": ", message, "", -1, -1);
         System.out.println(house.getName() + ": " + message);
         break;
       case "bid":
@@ -63,16 +63,16 @@ public class AuctionHouseProtocol
         {
           message = "declined";
         }
-        response = new Message(this, house.getName() + ": ", message, request.getItem(), request.getKey(), request.getAmount());
+        response = new Message(house, house.getName() + ": ", message, request.getItem(), request.getKey(), request.getAmount());
         System.out.println(house.getName() + ": " + message);
         break;
       case "confirmation":
         message = "ignore";
-        response = new Message(null, house.getName() + ": ", message, "", -1, -1);
+        response = new Message(house, house.getName() + ": ", message, "", -1, -1);
         break;
       default:
         message = "Error - request not recognized.";
-        response = new Message(null, house.getName() + ": ", message, "", -1, -1);
+        response = new Message(house, house.getName() + ": ", message, "", -1, -1);
         System.out.println(house.getName() + ": " + message);
         break;
     }
